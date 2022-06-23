6e794ea4da16359b2f7722dfa955abf115c3c1c9
Faisal Rehman <faisal.rehman22@hotmail.com>
2022-06-23 12:59:16 +0500

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for AchiStar Technologies";
+        return "This small DevOps Project is being worked on by Faisal & Nabeel";
     }
 }
